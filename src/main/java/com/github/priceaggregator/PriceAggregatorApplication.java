2c1d213449912fbeff6a5bf630c08ab1f9ba2e3b
Lexa <a.pisarenko9001@gmail.com>
2022-11-04 22:49:05 +0700

    Init commit

---
+++ src/main/java/com/github/priceaggregator/PriceAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.priceaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/priceaggregator/PriceAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.priceaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
